a31879a1b57eab496ee700097da39760ea2915b6
dzikrisyairozi <dikiogres@gmail.com>
2025-02-07 19:29:44 +0700

    feat: add family interface

---
+++ src/types/family.ts
@@ -0,0 +1,43 @@
+export interface Person {
+  id: number;
+  shortName: string;
+  fullName: string;
+  age: number;
+  gender: string;
+  status: 'alive' | 'deceased';
+  phone: string;
+  address: string;
+  parents: {
+    id: number;
+    type: 'biological' | 'adoptive';
+  }[];
+  spouses: {
+    id: number;
+    status: 'current' | 'deceased' | 'divorced';
+  }[];
+  children: {
+    id: number;
+    type: 'biological' | 'adoptive';
+  }[];
+}
+
+export interface PersonFormData {
+  shortName: string;
+  fullName: string;
+  age: string;
+  gender: string;
+  status: string;
+  phone: string;
+  address: string;
+  parents: { id: string; type: 'biological' | 'adoptive' }[];
+  spouses: { id: string; status: 'current' | 'deceased' | 'divorced' }[];
+  children: { id: string; type: 'biological' | 'adoptive' }[];
+}
+
+export interface FamilyMember extends Person {
+  children: {
+    id: number;
+    type: 'biological' | 'adoptive';
+    member: FamilyMember;
+  }[];
+}
